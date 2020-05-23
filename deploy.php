b14e6754dbbc89a041460cb3988ec3f4fb00218a
神谷友樹 <jetree900@gmail.com>
2020-05-23 21:04:24 +0900

    sshのキーを確認

--- deploy.php
+++ deploy.php
@@ -4,6 +4,7 @@ namespace Deployer;
 require_once __DIR__ . '/vendor/autoload.php';
 
 require 'recipe/common.php';
+use YowayowaEnginners\SlackChannelProtector\Env;
 
 // Project name
 set('application', 'slack-channel-protector');
@@ -23,30 +24,34 @@ set('shared_dirs', []);
 // Writable dirs by web server 
 set('writable_dirs', []);
 
-set('ssh_multiplexing', true);
-
 // Hosts
 
-host(getenv('HOSTNAME'))
-    ->user(getenv('LOGINUSER'))
-    ->port(getenv('PORT'))
+// host(getenv('HOSTNAME'))
+//     ->user(getenv('LOGINUSER'))
+//     ->port(('PORT'))
+//     ->stage('production')
+//     ->set('deploy_path', '~/slack-channel-protector');
+
+host(Env::getEnvValue('HOSTNAME'))
+    ->user(Env::getEnvValue('LOGINUSER'))
+    ->port(Env::getEnvValue('PORT'))
     ->stage('production')
-    ->set('deploy_path', '/slack-channel-protector');
+    ->set('deploy_path', '~/slack-channel-protector');
 
 // Tasks
 
 desc('Deploy your project');
 task('deploy', [
     'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
+    // 'deploy:prepare',
+    // 'deploy:lock',
+    // 'deploy:release',
+    // 'deploy:update_code',
+    // 'deploy:shared',
+    // 'deploy:writable',
+    // 'deploy:vendors',
+    // 'deploy:clear_paths',
+    // 'deploy:symlink',
     'deploy:unlock',
     'cleanup',
     'success'
